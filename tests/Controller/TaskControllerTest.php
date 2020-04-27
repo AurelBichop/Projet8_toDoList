3e2d5649459b2c1981fe495bddd73590b6159783
Aurelien <linux.aurelien@gmail.com>
2020-04-27 15:15:33 +0200

    ajout des test  pour le CRUD des taches

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -13,11 +13,32 @@ class TaskControllerTest extends WebTestCase
     use CreateUser;
     use UserLogin;
 
+    /**
+     * Création d'un utlisateur qui a une session de connection
+     * @return void
+     */
+    private function userConnected():void
+    {
+        //Création de l'utilisateur
+        $user = $this->userFixture($this->em, $this->encoder);
+        //Creation de la session avec le token de connection
+        $this->login($this->client, $user);
+    }
+
+
+    /**
+     * Permet de creer un Tache fixture
+     * @param string $titre
+     * @param string $content
+     * @param bool $isDone
+     * @return Task
+     */
     private function createTask(string $titre = 'Ma premiere Tache',string $content = 'contenue de tache',$isDone = false):Task
     {
         $task = new Task();
         $task->setTitle($titre)
-             ->setContent($content);
+             ->setContent($content)
+             ->setCreatedAt(new \DateTime());
 
         if($isDone){
             $task->toggle(true);
@@ -41,10 +62,8 @@ class TaskControllerTest extends WebTestCase
 
         $listTaskNotFinish = [$task1,$task2,$task3];
 
-        //Création de l'utilisateur
-        $user = $this->userFixture($this->em, $this->encoder);
-        //Creation de la session avec le token de connection
-        $this->login($this->client, $user);
+        //Création et connection de l'utilisateur
+        $this->userConnected();
 
         //Act
         $crawler = $this->client->request('GET', '/');
@@ -76,10 +95,8 @@ class TaskControllerTest extends WebTestCase
 
         $listTaskFinish = [$task1,$task2,$task3];
 
-        //Création de l'utilisateur
-        $user = $this->userFixture($this->em, $this->encoder);
-        //Creation de la session avec le token de connection
-        $this->login($this->client, $user);
+        //Création et connection de l'utilisateur
+        $this->userConnected();
 
         //Act
         $crawler = $this->client->request('GET', '/');
@@ -104,12 +121,11 @@ class TaskControllerTest extends WebTestCase
      * @test
      */
     public function list_of_task_finish(){
-        //Création de l'utilisateur
-        $user = $this->userFixture($this->em, $this->encoder);
-        //Creation de la session avec le token de connection
-        $this->login($this->client, $user);
 
-        //3 tâches fini
+        //Création et connection de l'utilisateur
+        $this->userConnected();
+
+        //tâches fini
         $task1 = $this->createTask('Premiere Tache fini',' contenu tache fini',true);
         $task2 = $this->createTask('Deuxieme Tache fini', 'Faire ma vidange',true);
         $task3 = $this->createTask('Troisime tache fini', 'réinstallation de mon serveur web',true);
@@ -142,17 +158,16 @@ class TaskControllerTest extends WebTestCase
     {
         //Arrange
         $task1 = $this->createTask();
-        //Création de l'utilisateur
-        $user = $this->userFixture($this->em, $this->encoder);
-        //Creation de la session avec le token de connection
-        $this->login($this->client, $user);
+
+        //Création et connection de l'utilisateur
+        $this->userConnected();
 
         //Act
         $crawler = $this->client->request('GET', '/task');
         //clique sur le bouton du formulaire
         $form = $crawler->selectButton('Marquer comme faite')->form();
         $this->client->submit($form);
-        $crawler = $this->client->followRedirect();
+        $this->client->followRedirect();
 
         $responseContent = $this->client->getResponse()->getContent();
 
@@ -163,6 +178,87 @@ class TaskControllerTest extends WebTestCase
         $this->assertStringNotContainsString($task1->getContent(), $responseContent);
     }
 
-    //A refaire en  vérifiant en bdd mofification toggle Task
-    //$this->assertFalse($task1->isDone());
+    /**
+     * @test
+     */
+    public function create_task_action()
+    {
+        //Création et connection de l'utilisateur
+        $this->userConnected();
+
+        //Act
+        $crawler = $this->client->request('GET', '/tasks/create');
+
+        $form = $crawler->selectButton('Ajouter')->form([
+            'task[title]' => 'Tache test',
+            'task[content]' => 'je suis une tache generé par un test fonctionnel'
+        ]);
+        $this->client->submit($form);
+
+        $this->assertResponseRedirects('/task');
+        $this->client->followRedirect();
+        $responseContent = $this->client->getResponse()->getContent();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('.alert.alert-success');
+        $this->assertStringContainsString('Tache test', $responseContent);
+        $this->assertStringContainsString('je suis une tache generé par un test fonctionnel', $responseContent);
+    }
+
+    /**
+     * @test
+     */
+    public function edit_task_action()
+    {
+        //Création d'une tache à éditer
+        $task1 = $this->createTask();
+        //Création et connection de l'utilisateur
+        $this->userConnected();
+
+        //Act
+        $crawler = $this->client->request('GET', '/tasks/'.$task1->getId().'/edit');
+        $responseContent = $this->client->getResponse()->getContent();
+
+        //check le contenu du formulaire d'edition
+        $this->assertStringContainsString($task1->getTitle(), $responseContent);
+        $this->assertStringContainsString($task1->getContent(), $responseContent);
+
+        $form = $crawler->selectButton('Modifier')->form([
+            'task[title]' => 'Tache modifié',
+            'task[content]' => 'je suis une tache Mofifié par un test'
+        ]);
+        $this->client->submit($form);
+        $this->client->followRedirect();
+        $responseContent = $this->client->getResponse()->getContent();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('.alert.alert-success');
+        $this->assertStringContainsString('Tache modifié', $responseContent);
+        $this->assertStringContainsString('je suis une tache Mofifié par un test', $responseContent);
+    }
+
+    /**
+     * @test
+     */
+    public function delete_task_action(){
+        //Création d'une tache à éditer
+        $task1 = $this->createTask();
+        //Création et connection de l'utilisateur
+        $this->userConnected();
+
+        //Act
+        $crawler = $this->client->request('GET', '/task');
+        //clique sur le bouton du formulaire
+        $form = $crawler->selectButton('Supprimer')->form();
+        $this->client->submit($form);
+        $this->client->followRedirect();
+
+        $responseContent = $this->client->getResponse()->getContent();
+
+        //Assert
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists(".alert.alert-success");
+        $this->assertStringNotContainsString($task1->getTitle(), $responseContent);
+        $this->assertStringNotContainsString($task1->getContent(), $responseContent);
+    }
 }
