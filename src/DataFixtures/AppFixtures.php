7fda9e929ba60b531f8ae636ea9dc1bcaccdaf30
Aurelien <linux.aurelien@gmail.com>
2020-05-08 18:08:39 +0200

    ajout de la création d'un admin et user pour les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,24 +2,67 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Role;
 use App\Entity\Task;
+use App\Entity\User;
 use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr-FR');
 
+        //creation du role Admin
+        $roleAdmin = new Role();
+        $roleAdmin->setTitle("ROLE_ADMIN");
+        $manager->persist($roleAdmin);
+
+        //création des utilisateurs
+        $user = new User();
+        $user->setUsername("UserName")
+             ->setEmail("usermail@exemple.com")
+             ->setPassword($this->encoder->encodePassword($user,'password'));
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setUsername('AurelAdmin')
+            ->setEmail("jsuisadmin@dansmatete.com")
+            ->addRole($roleAdmin)
+            ->setPassword($this->encoder->encodePassword($admin,'passwordAdmin'));
+        $manager->persist($admin);
+
+        $tabUsers = [$user,$admin];
+
+        //création des taches liée aux utilisateurs + anonyme
         for ($i=0; $i<10; $i++){
             $task = new Task();
             $task->setTitle($faker->text(mt_rand(5,20)))
                  ->setContent($faker->text(mt_rand(20,120)))
+                 ->setAuthor($tabUsers[mt_rand(0,1)])
                  ->setCreatedAt($faker->dateTimeBetween('-10 days', 'now'));
 
             $manager->persist($task);
+        }
+
+        //Création des taches anonymes
+        for ($i=0; $i<5; $i++){
+            $taskAnonyme = new Task();
+            $taskAnonyme->setTitle($faker->text(mt_rand(5,20)))
+                ->setContent($faker->text(mt_rand(20,120)))
+                ->setCreatedAt($faker->dateTimeBetween('-10 days', 'now'));
+
+            $manager->persist($taskAnonyme);
         }
 
         $manager->flush();
--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -3,6 +3,8 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Role;
+use App\Entity\Task;
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
@@ -15,12 +17,94 @@ class UserTest extends TestCase
     {
         $user = new User();
 
-        $roles = $user->getRoles();
+        $rolesUser = $user->getRoles();
 
-        $checkRoleUser = array_search("ROLE_USER",$roles);
-        $checkRoleAdmin = array_search("ROLE_ADMIN",$roles);
+        $checkRoleUser = array_search("ROLE_USER",$rolesUser);
+        $checkRoleAdmin = array_search("ROLE_ADMIN",$rolesUser);
 
         $this->assertIsNotBool($checkRoleUser);
         $this->assertFalse($checkRoleAdmin);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_with_admin_role()
+    {
+        $user = new User();
+
+        $roleAdmin = new Role();
+        $roleAdmin->setTitle("ROLE_ADMIN");
+
+        $user->addRole($roleAdmin);
+        $rolesUser = $user->getRoles();
+
+        $this->assertCount(2,$rolesUser);
+        $this->assertSame("ROLE_USER",$user->getRoles()[1]);
+        $this->assertSame("ROLE_ADMIN",$user->getRoles()[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function remove_admin_role()
+    {
+        $user = new User();
+
+        $roleAdmin = new Role();
+        $roleAdmin->setTitle("ROLE_ADMIN");
+
+        $user->addRole($roleAdmin);
+        $user->removeRole($roleAdmin);
+
+        $rolesUser = $user->getRoles();
+        $checkRoleAdmin = array_search("ROLE_ADMIN",$rolesUser);
+
+        $this->assertCount(1,$rolesUser);
+        $this->assertFalse($checkRoleAdmin);
+    }
+
+    /**
+     * @test
+     */
+    public function add_a_task()
+    {
+        $user = new User();
+
+        $task = new Task();
+        $task->setTitle("Tache de test")
+             ->setContent("contenu de la tache");
+
+        $task2 = new Task();
+        $task2->setTitle("Tache de test2")
+            ->setContent("contenu de la tache2");
+
+        $user->addTask($task)
+             ->addTask($task2);
+
+        $this->assertSame($task->getTitle(),$user->getTasks()[0]->getTitle());
+        $this->assertSame($task->getContent(),$user->getTasks()[0]->getContent());
+        $this->assertSame($task2->getTitle(),$user->getTasks()[1]->getTitle());
+        $this->assertSame($task2->getContent(),$user->getTasks()[1]->getContent());
+    }
+
+    /**
+     * @test
+     */
+    public function remove_a_task()
+    {
+        $user = new User();
+
+        $task = new Task();
+        $task->setTitle("Tache de test")
+            ->setContent("contenu de la tache");
+
+        $user->addTask($task);
+
+        $taskUser = $user->getTasks();
+        $this->assertCount(1,$taskUser);
+
+        $user->removeTask($task);
+        $this->assertCount(0,$taskUser);
     }
 }
